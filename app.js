9dd9e023fcb2f24ad363766526993c0e3f6845eb
razvanr91 <razvan.roman91@outlook.com>
2021-11-11 16:02:05 +0000

    Add data for people who lost the battle

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // const { default: axios } = require("axios");
 
+let numberOfDays = 60;
+let count = 0;
+
 const ctx = document.getElementById('chart').getContext('2d');
 let data;
 let countryCode = 'RO';
@@ -24,28 +27,55 @@ data = await axios.request(options).then(response => {
 console.log(data);
 let days = [];
 let cases = [];
-
+let deaths = [];
 let timeline = data.timeline;
 
-timeline.forEach(day => {
+// timeline.forEach(day => {
+//     days.push(day.date);
+//     cases.push(day.new_confirmed);
+//     deaths.push(day.new_deaths)
+//     count++;
+//     if (count === 30) {
+//         return;
+//     }
+// })
+
+for(let day of timeline) {
     days.push(day.date);
     cases.push(day.new_confirmed);
-})
+    deaths.push(day.new_deaths)
+    count++;
+    if (count === numberOfDays) {
+        break;
+    }
+}
+
+
+let updatedAt = new Date(data.updated_at).toDateString();
+console.log(updatedAt);
 
 days.reverse();
 cases.reverse();
-console.log(days)
+deaths.reverse();
+console.log(deaths)
 
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: days,
         datasets: [{
-            label: '# of Votes',
+            label: [`# of new confirmed cases as of ${updatedAt}`],
             data: cases,
             fill: true,
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }, {
+            label: `# of deaths as of ${updatedAt}`,
+            data: deaths,
+            fill: true,
+            backgroundColor: 'rgba(0,0,0)',
+            borderColor: 'rgb(255, 0, 55)',
             borderWidth: 1
         }]
     },
